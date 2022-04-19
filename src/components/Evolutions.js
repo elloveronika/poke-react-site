183460f8a216f519a8c798c0ac3abc312fcfa7b6
Xariwey <93450968+Xariwey@users.noreply.github.com>
2022-04-19 17:46:15 -0500

    Update Evolutions.js
    .

--- src/components/Evolutions.js
+++ src/components/Evolutions.js
@@ -1,3 +1,11 @@
-export default function Evolution() {
-  return <h1>this is Evolution component</h1>;
+export default function Evolution({props}) {
+
+  async function getPokeSprites(pokeName) {
+    console.log("this is inside ßpokeSprites func", props.pokeEvos);
+  }
+
+
+  return (
+    <h1>`This es the names of the Evolutions : ${pokeEvos}`</h1>
+  );
 }
